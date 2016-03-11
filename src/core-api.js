a6031af2adfec82693a5347df3d3773602d9b227
Pete Wildsmith <pete@dabapps.com>
2016-03-11 09:26:55 +0000

    package setup

--- src/core-api.js
+++
@@ -1,9 +0,0 @@
-import "babel/polyfill";
-import { transition } from "./transport";
-import {CoreArray, CoreDocument} from './document';
-
-export default function get (url) {
-    return transition(url, 'follow');
-}
-
-export {CoreArray, CoreDocument};
