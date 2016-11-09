b5a8ee0b55b8542b1ae7c7721417f24672825e31
Emmanouil Konstantinidis <hello@manos.im>
2016-11-09 10:51:50 +0000

    Setup Transport & Decoder

--- tests/basic.js
+++ tests/basic.js
@@ -6,8 +6,20 @@ const client = coreapi.client();
 client.get("http://httpbin.org/")
   .then((statusText) => {
     expect(statusText).toEqual("OK");
-    console.log('Passed ✔︎');
+    console.log('Get: Passed ✔︎');
   })
   .catch((error) => {
     expect(error).toNotExist();
   });
+
+client.transport.action("http://httpbin.org/", client.decoder)
+  .then((statusText) => {
+    expect(statusText).toEqual("OK");
+    console.log('Transport: Passed ✔︎');
+  })
+  .catch((error) => {
+    expect(error).toNotExist();
+  });
+
+expect(client.decoder.decode("Hello")).toEqual("Hello");
+console.log('Decoder: Passed ✔︎');
