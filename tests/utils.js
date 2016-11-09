96d0e9beed37c8a5a7327d1c21004351fcdc9d96
Emmanouil Konstantinidis <hello@manos.im>
2016-11-09 17:09:31 +0000

    Test utils/negotiateDecoder

--- tests/utils.js
+++ tests/utils.js
@@ -15,9 +15,35 @@ describe('Test the Utils/determineTransport function', function () {
   })
 
   it('should throw an error if it is an unsupported url', function () {
-    const doDetermine = function (url) {
-      return utils.determineTransport([new coreapi.transports.HTTPTransport()], url)
-    }
+    const doDetermine = url => utils.determineTransport([new coreapi.transports.HTTPTransport()], url)
+    expect(doDetermine).toThrow()
+  })
+})
+
+describe('Test the Utils/negotiateDecoder function', function () {
+  const decoders = new coreapi.Client().decoders
+
+  it('should return the default decoder if content type is undefined', function () {
+    const contentType = undefined
+    const result = utils.negotiateDecoder(decoders, contentType)
+    expect(result instanceof coreapi.codecs.CoreJSONCodec).toBeTruthy()
+  })
+
+  it('should return the decoder for a content type (application/json)', function () {
+    const contentType = 'application/json'
+    const result = utils.negotiateDecoder(decoders, contentType)
+    expect(result instanceof coreapi.codecs.JSONCodec).toBeTruthy()
+  })
+
+  it('should return the decoder for a content type (text/html)', function () {
+    const contentType = 'text/html'
+    const result = utils.negotiateDecoder(decoders, contentType)
+    expect(result instanceof coreapi.codecs.TextCodec).toBeTruthy()
+  })
+
+  it('should throw an error if it is an unsupported url', function () {
+    const contentType = 'hello/world'
+    const doDetermine = url => utils.negotiateDecoder(decoders, contentType)
     expect(doDetermine).toThrow()
   })
 })
