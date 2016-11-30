e8af8bd4a136507ba921dea2417c57157b215d08
Emmanouil Konstantinidis <hello@manos.im>
2016-11-30 11:59:24 +0000

    Change Transport.action signature

--- tests/transports/http.js
+++ tests/transports/http.js
@@ -1,5 +1,6 @@
 const transports = require('../../lib/transports')
 const codecs = require('../../lib/codecs')
+const document = require('../../lib/document')
 const testUtils = require('../__helpers__/utils')
 
 describe('Test the HTTPTransport', function () {
@@ -7,9 +8,10 @@ describe('Test the HTTPTransport', function () {
 
   it('should check the action function of an HTTP transport (text/html)', function () {
     const url = 'http://www.example.com/'
+    const link = new document.Link(url)
     const transport = new transports.HTTPTransport(testUtils.mockedFetch('Hello, world', 'text/html'))
 
-    return transport.action(url, decoders)
+    return transport.action(link, decoders)
       .then((res) => {
         expect(res).toEqual('Hello, world')
       })
@@ -17,28 +19,30 @@ describe('Test the HTTPTransport', function () {
 
   it('should check the action function of an HTTP transport (json)', function () {
     const url = 'http://www.example.com/'
+    const link = new document.Link(url)
     const transport = new transports.HTTPTransport(testUtils.mockedFetch('{"text": "Hello, world"}', 'application/json'))
 
-    return transport.action(url, decoders)
-      .then((res) => {
-        expect(res).toEqual({text: 'Hello, world'})
-      })
+    return transport.action(link, decoders)
+      .then(res => expect(res).toEqual({text: 'Hello, world'}))
   })
 
   it('should check the action function of an HTTP transport (network fail)', function () {
     const url = 'http://www.example.com/'
+    const link = new document.Link(url)
     const transport = new transports.HTTPTransport(testUtils.mockedFetch('ERROR', 'text/html', 500))
-    expect(transport.action(url, decoders).then(() => {}).catch(() => {})).toThrow()
+
+    expect(transport.action(link, decoders).then(() => {}).catch(() => {})).toThrow()
   })
 
   it('should check the action function of an HTTP transport (json) with query params', function () {
     const url = 'http://www.example.com/'
+    const link = new document.Link(url)
     const transport = new transports.HTTPTransport(testUtils.echoUrl)
     const params = {
       page: 23
     }
 
-    return transport.action(url, decoders, params)
+    return transport.action(link, decoders, params)
       .then((res) => {
         expect(res).toEqual({url: 'http://www.example.com/?page=23'})
       })
